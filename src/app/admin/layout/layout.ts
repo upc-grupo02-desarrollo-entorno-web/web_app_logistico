8df7718c91a265f3f5467ca687677219da35dce8
Jack Moreno <jack12972@gmail.com>
2026-07-06 19:24:33 -0500

    add layout admin

--- src/app/admin/layout/layout.ts
+++ src/app/admin/layout/layout.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarApp } from '../../shared/navbar-app/navbar-app';
+import { SidebarAdmin } from '../../shared/sidebar-admin/sidebar-admin';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterOutlet, NavbarApp, SidebarAdmin],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
--- src/app/shared/sidebar-admin/sidebar-admin.ts
+++ src/app/shared/sidebar-admin/sidebar-admin.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar-admin',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidebar-admin.html',
   styleUrl: './sidebar-admin.css',
 })
