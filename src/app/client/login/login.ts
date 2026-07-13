69647ddf91bde2181882633a79b174e716d484c2
Greysi Ticona <greciafrens52@gmail.com>
2026-07-12 22:46:49 -0500

    login real

--- src/app/client/login/login.ts
+++ src/app/client/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { Auth } from '../../core/services/auth';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './login.css',
 })
 export class Login {
+
   usuario = '';
-  clave   = '';
+  clave = '';
+  error = false;
+
+  constructor(private auth: Auth, private router: Router) {}
+
+  ingresar(): void {
+    const usuarioAutenticado = this.auth.login(this.usuario, this.clave);
+
+    if (!usuarioAutenticado) {
+      this.error = true;
+      return;
+    }
+
+    this.error = false;
+    const destino = usuarioAutenticado.rol === 'admin' ? '/admin/dashboard' : '/cliente/dashboard';
+    this.router.navigate([destino]);
+  }
 }
--- src/app/shared/navbar-app/navbar-app.ts
+++ src/app/shared/navbar-app/navbar-app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { Auth } from '../../core/services/auth';
 
 @Component({
   selector: 'app-navbar-app',
@@ -7,4 +8,20 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar-app.html',
   styleUrl: './navbar-app.css',
 })
-export class NavbarApp {}
+export class NavbarApp {
+
+  constructor(private auth: Auth, private router: Router) {}
+
+  get usuario() {
+    return this.auth.obtenerUsuarioActual();
+  }
+
+  get inicio(): string {
+    return this.usuario?.rol === 'admin' ? '/admin/dashboard' : '/cliente/dashboard';
+  }
+
+  salir(): void {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
+}
