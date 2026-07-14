761e19e885e2d12fd5c305cfa7e690d90713dcd6
Greysi Ticona <greciafrens52@gmail.com>
2026-07-14 09:13:37 -0500

    feat: navbar, layout cliente y dashboard admin con datos reales

--- src/app/admin/dashboard-admin/dashboard-admin.ts
+++ src/app/admin/dashboard-admin/dashboard-admin.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DespachosService } from '../../core/services/despachos';
+import { ContenedoresService } from '../../core/services/contenedores';
+import { Incidencias } from '../../core/services/incidencias';
 
 @Component({
   selector: 'app-dashboard-admin',
@@ -6,4 +9,23 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard-admin.html',
   styleUrl: './dashboard-admin.css',
 })
-export class DashboardAdmin {}
+export class DashboardAdmin implements OnInit {
+
+  totalDespachos = 0;
+  contenedoresActivos = 0;
+  incidenciasAbiertas = 0;
+
+  constructor(
+    private despachosService: DespachosService,
+    private contenedoresService: ContenedoresService,
+    private incidenciasService: Incidencias
+  ) {}
+
+  ngOnInit(): void {
+    this.totalDespachos = this.despachosService.obtenerTodos().length;
+    this.contenedoresActivos = this.contenedoresService.obtenerTodos()
+      .filter(c => c.estado === 'En uso').length;
+    this.incidenciasAbiertas = this.incidenciasService.obtenerTodas()
+      .filter(i => i.estado === 'Abierta').length;
+  }
+}
