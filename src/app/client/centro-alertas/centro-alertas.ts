29f5602be5a4bb4b7c654d50fcf339a636b26622
Angel Tica <atica02@gmail.com>
2026-07-13 21:54:24 -0500

    Actualice centro-alertas

--- src/app/client/centro-alertas/centro-alertas.ts
+++ src/app/client/centro-alertas/centro-alertas.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Alerta } from '../../core/models/alerta.model';
+import { AlertasService } from '../../core/services/alertas';
 
 @Component({
   selector: 'app-centro-alertas',
@@ -6,4 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './centro-alertas.html',
   styleUrl: './centro-alertas.css',
 })
-export class CentroAlertas {}
+export class CentroAlertas implements OnInit {
+
+  alertas: Alerta[] = [];
+
+  constructor(private alertasService: AlertasService) {}
+
+  ngOnInit(): void {
+    this.alertas = this.alertasService.obtenerTodas();
+  }
+
+  icono(tipo: string): string {
+    switch (tipo) {
+      case 'warning': return 'bi-exclamation-triangle-fill text-warning';
+      case 'success': return 'bi-check-circle-fill text-success';
+      default:        return 'bi-info-circle-fill text-info';
+    }
+  }
+}
